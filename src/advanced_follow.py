a0552e96991b3a54a1cd45ea84aff06c1a91bb8a
danbing <danbing123@gmail.com>
2019-12-16 14:21:34 -0500

    Config AMCL

--- src/advanced_follow.py
+++ src/advanced_follow.py
@@ -3,19 +3,22 @@
 import rospy
 import yaml
 import os
+import tf2_ros
+import actionlib
+import move_base
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Time
 from stalkerbot.msg import location_info
 from fiducial_msgs.msg import FiducialTransformArray
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
+
 class Follow():
 
     '''Sets values for linear and rotational velocity'''
     def _location_cb(self, msg):
-        self._location = msg
+        self._location = msgupdate frame 103
 
-        '''Depreciated after adopting move_base'''
         # linear_vel = msg.z_translation * 0.12
         # rotational_vel = msg.x_translation/msg.z_translation * self._COEFFICIENT_ROTATIONAL_VELOCITY
         # if linear_vel > self._MAXIMUM_LINEAR_VELOCITY:
@@ -66,50 +69,52 @@ class Follow():
         interval_subscriber = rospy.Subscriber('/stalkerbot/fiducial/interval', Time, self._interval_cb, queue_size=1)
 
         _tfBuffer = tf2_ros.Buffer()
-        _listener = tf2_ros.TransformListener(self._tfBuffer)
+        _listener = tf2_ros.TransformListener(_tfBuffer)
         _client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-        client.wait_for_server()
+        _client.wait_for_server()
+        self._trans = None
 
         _goal = MoveBaseGoal()
         _goal.target_pose.header.frame_id = "map"
         
         while not rospy.is_shutdown():
+            try:
+                self._trans = _tfBuffer.lookup_transform('map', 'target', rospy.Time())
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException,tf2_ros.ExtrapolationException):
+                print "!!!"
+                self._rate.sleep()
+                return
             '''if the marker is 'warm' and distance is bigger than desired distance
             AND interval is initiated and does not exceed detection buffer (not detecting marker for too long)'''
-            if self._warm \
-                and self._location is not None and self._location.z_translation > self._DESIRED_DISTANCE \
-                and self._interval is not None and self._interval.data.secs < self._DETECTION_BUFFER_SEC:
+            if self._warm and self._location is not None and self._interval is not None and self._trans is not None\
+                and self._location.z_translation > self._DESIRED_DISTANCE and self._interval.data.secs < self._DETECTION_BUFFER_SEC:
 
                 '''Depreciated after adopting move_base'''
                 # if self._interval.data.secs > self._MOVEMENT_BUFFER_SEC \
                 #     or (self._interval.data.secs == self._MOVEMENT_BUFFER_SEC and self._interval.data.nsecs < self._MOVEMENT_BUFFER_NANOSEC):
                 #     self._twist.angular.z *= self._COEFFICIENT_ROTATIONAL_VELOCITY_BUFFER
 
-                goal.target_pose.header.stamp = rospy.Time.now()
-                goal.target_pose.pose.position.x = trans.transform.translation.x
-                goal.target_pose.pose.position.y = trans.transform.translation.y 
-                goal.target_pose.pose.position.z = trans.transform.translation.z 
+                '''
+                Sends goal to move_base of ros navigation
+                '''
+                _goal.target_pose.header.stamp = rospy.Time.now()
 
-                orientation_q = msg.transform.transform.rotation
-                orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-                (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
-                roll = 0
-                pitch = 0 
-                yaw = math.atan2(trans.transform.translation.y, trans.transform.translation.x)
-                q = quaternion_from_euler(roll, pitch, yaw)
+                _goal.target_pose.pose.position.x = self._trans.transform.translation.x
+                _goal.target_pose.pose.position.y = self._trans.transform.translation.y 
+                _goal.target_pose.pose.position.z = self._trans.transform.translation.z 
 
-                goal.target_pose.pose.orientation.x = q[0]
-                goal.target_pose.pose.orientation.y = q[1]
-                goal.target_pose.pose.orientation.z = q[2]
-                goal.target_pose.pose.orientation.w = q[3]
-
-                client.send_goal(goal)
+                _goal.target_pose.pose.orientation.x = 0
+                _goal.target_pose.pose.orientation.y = 0
+                _goal.target_pose.pose.orientation.z = 0
+                _goal.target_pose.pose.orientation.w = 1
+                _client.send_goal(_goal)
 
             elif not self._warm:
                 # TODO: An action which steers the robot to the correct position and will be interrupted immediately upon detection of another warm marker
                 continue
 
             else:
+                _client.cancel_all_goals()
                 cmd_vel.publish(Twist())
             self._rate.sleep()
 
